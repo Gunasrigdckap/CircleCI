37c6c7badd1f4f89a365d8c588483af0cbaa3711
GunasriG <gunasrig@dckap.com>
2025-04-14 16:53:05 +0530

    Remove tests.py

--- tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestHello(unittest.TestCase):
-    def test_say_hello(self):
-        from main import say_hello
-        self.assertEqual(say_hello("Gunasri"), "Hello, Gunasri!")
-
-if __name__ == '__main__':
-    unittest.main()
