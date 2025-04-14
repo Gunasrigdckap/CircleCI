854848003fa18ccabd273deea9012806697a275f
GunasriG <gunasrig@dckap.com>
2025-04-14 16:46:57 +0530

    tests file changed

--- tests.py
+++ tests.py
@@ -1,2 +1,9 @@
-def say_hello(name):
-    print(f"Hello, {name}!")
+import unittest
+
+class TestHello(unittest.TestCase):
+    def test_say_hello(self):
+        from main import say_hello
+        self.assertEqual(say_hello("Gunasri"), "Hello, Gunasri!")
+
+if __name__ == '__main__':
+    unittest.main()
